7b5994a3d3f821850d20a418c9d4833be8767872
dora35 <2572988477@qq.com>
2018-06-08 12:17:17 +0800

    first

--- function_practice.py
+++ function_practice.py
@@ -1,4 +1,4 @@
-#question 1 
+print question 1 
 def f(x):
     return 6 * x - 4
 print(f(4))
@@ -6,10 +6,37 @@ print(f(5))
 print(f(6))
 print(f(7))
 
-#question 2
-def g(x):
-    return x + 4
-print(g(4))
-print(g(5))
-print(g(6))
-print(g(7))
+print question 2
+def f(n):
+    return  = 1 / (3**n-1)
+print(f(1))
+print(f(2))
+print(f(3))
+print(f(4))
+print(f(5))
+
+print question 3
+def f(n):
+	return = (-1**n) * (3)
+print(f(1))
+print(f(2))
+print(f(3))
+print(f(4))
+print(f(5))
+
+print Functions_worksheet
+print question a
+
+def m(h):
+	return = 1000 + (100 * h)
+
+print question b
+def b(m):
+	return m / 20
+
+print question c
+def m(k):
+	return 1000 + 100 * k
+def bm[m(k)]:
+	return m(k) / 20
+
