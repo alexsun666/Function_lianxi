ecae2bd6bddf3b418acbb5b1f73ef177c5782f7f
Kalu Kalu <1718221904@qq.com>
2018-06-08 11:50:25 +0800

    first

---
+++ function_practice.py
@@ -0,0 +1,15 @@
+#question 1 
+def f(x):
+    return 6 * x - 4
+print(f(4))
+print(f(5))
+print(f(6))
+print(f(7))
+
+#question 2
+def g(x):
+    return x + 4
+print(g(4))
+print(g(5))
+print(g(6))
+print(g(7))
