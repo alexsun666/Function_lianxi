8ad87f88c805c044365c69445fbec2ce15b1f76e
alexsun666 <1309642783@qq.com>
2018-06-21 10:36:17 +0800

    first HW

--- function_practice.py
+++ function_practice.py
@@ -1,42 +1,80 @@
-print question 1 
+#question 1
 def f(x):
     return 6 * x - 4
 print(f(4))
 print(f(5))
 print(f(6))
+
+#question 2"
+def f(n):
+    return 1 / (3**n-1)
+print(f(1))
+print(f(2))
+print(f(3))
+print(f(4))
+print(f(5))
+#question 3"
+def f(n):
+	return (-1**n) * (3)
+print(f(1))
+print(f(2))
+print(f(3))
+print(f(4))
+print(f(5))
+
+# question 4
+def f(x):
+    f = (250*0.07*x) + 250
+    return f
+
+print(f(1))
+print(f(3))
 print(f(7))
+print(f(20))
 
-print question 2
-def f(n):
-    return  = 1 / (3**n-1)
+# question 5
+def f(x):
+    f = 325 * (1.04**x)
+    return f
+
 print(f(1))
-print(f(2))
 print(f(3))
-print(f(4))
-print(f(5))
+print(f(7))
+print(f(20))
+# question 6
+def bills(h):
+	ma = 10000 * (1.04**h)
+	mb = 10000 + (10000*0.05*h)
+	if ma - mb > 0
+	return (ma , mb)
+	
 
-print question 3
-def f(n):
-	return = (-1**n) * (3)
-print(f(1))
-print(f(2))
-print(f(3))
-print(f(4))
-print(f(5))
-
-print Functions_worksheet
-print question a
+#"Functions_worksheet"
+#'question a'
 
 def m(h):
-	return = 1000 + (100 * h)
+	return 1000 + (100 * h)
 
-print question b
+#'question b'
+
 def b(m):
 	return m / 20
 
-print question c
-def m(k):
-	return 1000 + 100 * k
-def bm[m(k)]:
-	return m(k) / 20
-
+#'question c'
+def bills_get(h):
+	salary = m(h) 
+	bills = b(salary)
+	return bills
+print (bills_get(10))
+#question d
+def bills_dina_earns_each_month(h):
+	a = 1000 + (100 * h)
+	bills_dina_earns_each_month = a / 20
+	return bills_dina_earns_each_month
+print(bills_dina_earns_each_month(5))
+#question e
+def money_dina_gets_in_a_month(number_of_bills):
+	money = number_of_bills * 20
+	hours_dina_teaches = (money - 1000) / 100
+	return hours_dina_teaches
+print(money_dina_gets_in_a_month(100))
